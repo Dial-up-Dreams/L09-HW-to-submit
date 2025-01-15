d4caeee5281fba846bce8e30d04cd14adea91bfc
Shara <168702435+Dial-up-Dreams@users.noreply.github.com>
2025-01-15 23:38:21 +0800

    part 2

--- src/components/Home.js
+++ src/components/Home.js
@@ -12,7 +12,7 @@ export default function Home({ title }) {
 
       <h1>{title}</h1>
       <p>
-       
+      If you are excited by the latest technological advances and have a passion for problem-solving, join us at RP School of Infocomm (SOI). We will help you discover your hidden technical skills and nurture your creative digital dreams. We offer six diploma programmes across a wide range of infocomm disciplines.
       </p>
     </div>
   );
